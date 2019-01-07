027e31fef9a83fc929acf4c8317af8fa1b480968
munfred <munfred@brandeis.edu>
2019-01-06 23:22:51 -0800

    Add make_miscmatch_map function

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kite',
+      version='0.0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/pachterlab/kite',
+      author='Eduardo da Veiga Beltrame',
+      author_email='beltrame@caltech.edu',
+      license='BSD-2',
+      packages=['kite'],
+      zip_safe=False)
